d95851da91f6f9e624b73cf1269b16731fee9bb8
Jonathan Prieto <jonathan.prieto@me.com>
2019-10-13 15:29:07 -0500

    Everything Ok - Small Corrections2

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
     Tree<string> tree;
-    auto resultado=tree.Resolver("33+1");
+    auto resultado=tree.Resolver("6*5+(4+2)^a");
     cout<<"Resultado: "<<resultado<<endl;
     return 0;
 }
